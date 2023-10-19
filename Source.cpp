379a7bc251c4bd638720b1a88a2b9d85b202256b
KuzmynMaksym <144415346+KuzmynMaksym@users.noreply.github.com>
2023-10-19 08:21:59 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+#include <Windows.h> //локалізація
+
+using namespace std;
+
+int main()
+
+{
+
+	//локалізація
+
+	SetConsoleCP(1251);
+
+	SetConsoleOutputCP(1251);
+
+	int A{}, B{};
+
+	cin >> A >> B;
+
+	if ((A % 2 != 0 && B % 2 == 0) || (B % 2 != 0 && A % 2 == 0)) cout << "істина";
+
+	else cout << "брехня";
+
+	return 0;
+
+}
